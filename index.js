934acbb195a13c2790a8b206d11e814dc15d1b86
Breno C <breno.cesa99.97@gmail.com>
2022-07-11 17:59:55 -0300

    Edit listen express

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = 3333;
 
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Listen on http://localhost:${PORT}`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
