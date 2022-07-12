3426533ad1480483a60b9890263a6a36b31b5c5f
Breno C <breno.cesa99.97@gmail.com>
2022-07-12 18:06:37 -0300

    Add Middleware Get Header

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const routes = require("./routes.js");
 
+const setCache = (req, res, next) => {
+  const period = 60 * 5;
+  if (req.method == "GET") {
+    res.set("Cache-control", `public, max-age=${period}`);
+  } else {
+    res.set("Cache-control", `no-store`);
+  }
+
+  next();
+};
+
+app.use(setCache);
 app.use(routes);
 
 app.listen(process.env.PORT || 3000, function (error) {
